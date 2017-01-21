0ab5aa51b985ffeaff18f93bee45422076bf4819
Deetpanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-01-20 23:50:34 -0500

    Made a very basic UI, implementing functionality to allow user to select image from photo library

--- Boilermake4/ViewController.swift
+++ Boilermake4/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,  UINavigationControllerDelegate {
 
+  @IBOutlet weak var photoImage: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    //print(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  //MARK: UIImagePickerControllerDelegate
+  
+  
+  //MARK: Actions
+  
+  @IBAction func loadImageFromPhotoLibrary(_ sender: UIButton) {
+    let imagePickerController = UIImagePickerController()
+    imagePickerController.sourceType = .photoLibrary
+    imagePickerController.delegate = self
+    
+    present(imagePickerController, animated: true, completion: nil)
+
+    
+  }
+  
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+
+
 
 
 }
